c18bd46c7138820d8581ed0d7d26f7db83e09690
SawsanAllam <sawsanallam.332@gmail.com>
2025-05-24 22:22:24 +0300

    first init

---
+++ 2-arguments.js
@@ -0,0 +1,13 @@
+// let args =[]
+function msgArgs(args){
+    if(args.length===0){
+        console.log("No argument");
+        
+    }else if(args.length===1){
+        console.log("Argument found");
+    }else if (args.length===2){
+        console.log("Arguments found");
+        
+    }
+}
+msgArgs(["a", "d"])
