ee5a3ee0ba695f7fbc0e8c822001508be4a9e48a
SawsanAllam <sawsanallam.332@gmail.com>
2025-05-24 22:33:46 +0300

    first init

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,15 +1,13 @@
-// let args =[]
+const args = process.argv.slice(2).join(' ');
 function msgArgs(args){
     if(args.length===0){
         console.log("No argument");
         
     }else if(args.length===1){
         console.log("Argument found");
-    }else if (args.length===2){
+    }else {
         console.log("Arguments found");
         
     }
 }
-msgArgs([])
-msgArgs(["a"])
-msgArgs(["a", "d"])
+msgArgs(args);
