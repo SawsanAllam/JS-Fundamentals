4c8691668d47b440bda6c0ad81133b75168a48e8
SawsanAllam <sawsanallam.332@gmail.com>
2025-05-24 22:37:35 +0300

    first init

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,11 @@
 const args = process.argv.slice(2).join(' ');
-function msgArgs(args){
-    if(args.length===0){
-        console.log("No argument");
-        
-    }else if(args.length===1){
-        console.log("Argument found");
-    }else {
-        console.log("Arguments found");
-        
-    }
+function msgArgs(argString) {
+  if (argString.length === 0) {
+    console.log("No argument");
+  } else if (argString.split(' ').length === 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
+  }
 }
 msgArgs(args);
